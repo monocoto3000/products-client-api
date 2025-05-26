a2fa7cde856c301c387663a90d47695bb4d3e715
monocoto3000 <128664121+monocoto3000@users.noreply.github.com>
2025-05-26 03:58:19 -0600

    DEV TEST

--- src/utils/swagger.ts
+++ src/utils/swagger.ts
@@ -6,7 +6,7 @@ const options = {
   definition: {
     openapi: '3.0.0',
     info: {
-      title: 'Products Client API - DEV Test #2',
+      title: 'Products Client API - DEV Test #2.0',
       version: '1.0.0',
       description: 'Consult manager for Products',
     },
